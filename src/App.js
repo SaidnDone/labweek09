a947c526ad9c8a2956286c2495dd437d09c3c3a6
Said Mohamed <warstander45@gmail.com>
2023-11-11 17:42:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,17 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Fullstack Development- I</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <h3>100997363</h3>
+        <h4>Said Mohamed</h4>
+        <h4>George Brown College, Toronto</h4>
       </header>
     </div>
   );
