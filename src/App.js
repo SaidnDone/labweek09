7dc1c7ead498ac6dad805f765f7cffd55d63b4aa
Said Mohamed <warstander45@gmail.com>
2023-11-11 18:00:31 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [showForm, setShowForm] = useState(true);
+  const [formData, setFormData] = useState({
+    email: '',
+    fullName: '',
+    address: '',
+    city: '',
+    province: '',
+    postalCode: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowForm(false);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +36,23 @@ function App() {
         <h4>Said Mohamed</h4>
         <h4>George Brown College, Toronto</h4>
       </header>
+
+      {showForm ? (
+        <form onSubmit={handleSubmit} className="data-entry-form">
+          {/* ... Input fields go here ... */}
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <div className="entered-info">
+          <h3>Entered Information:</h3>
+          <p>Email: {formData.email}</p>
+          <p>Full Name: {formData.fullName}</p>
+          <p>Address: {formData.address}</p>
+          <p>City: {formData.city}</p>
+          <p>Province: {formData.province}</p>
+          <p>Postal Code: {formData.postalCode}</p>
+        </div>
+      )}
     </div>
   );
 }
